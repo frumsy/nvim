86305d91d47c71c87afebbcca74db3db89039372
frumsy <frumsy@users.noreply.github.com>
2024-02-19 22:25:58 -0500

    reverb

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -2,17 +2,7 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_fallback = true }
-      end,
-      mode = '',
-      desc = 'Format buffer',
-    },
-  },
+  keys = {},
   -- Everything in opts will be passed to setup()
   opts = {
     -- Define your formatters
